bc930bdf83a4f1d5698751bc72602bb0f7d4912a
Giulio Albertazzi <albertazzi.giulio@gmail.com>
2026-03-06 16:57:18 +0100

    feat(frontend): type description for client model

---
+++ resources/js/types/client.ts
@@ -0,0 +1,35 @@
+export interface Client {
+    id: number;
+    company_name: string;
+    email: string;
+    phone: string;
+    address: string;
+    vat_number: string;
+}
+
+export const columns= [
+    {
+        key: 'id',
+        label: 'ID',
+    },
+    {
+        key: 'company_name',
+        label: 'Company Name',
+    },
+    {
+        key: 'email',
+        label: 'Email',
+    },
+    {
+        key: 'phone',
+        label: 'Phone',
+    },
+    {
+        key: 'address',
+        label: 'Address',
+    },
+    {
+        key: 'vat_number',
+        label: 'VAT Number',
+    },
+];
