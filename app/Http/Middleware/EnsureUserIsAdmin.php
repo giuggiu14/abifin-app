6d3a4ade661a6fa13a143d966e33d5b420cbf1e4
Giulio Albertazzi <albertazzi.giulio@gmail.com>
2026-03-06 11:04:29 +0100

    fix(middleware): wrong method call

--- app/Http/Middleware/EnsureUserIsAdmin.php
+++ app/Http/Middleware/EnsureUserIsAdmin.php
@@ -15,7 +15,7 @@ class EnsureUserIsAdmin
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if ($request->user()?->is_admin) {
+        if ($request->user()?->isAdmin()) {
             return $next($request);
         }
         abort(403, 'Admin reserved area.');
