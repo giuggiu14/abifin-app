296fbbaed2acd6fca29a1679caa0dc15514402ed
Giulio Albertazzi <albertazzi.giulio@gmail.com>
2026-03-07 18:39:49 +0100

    feat(frontend): pages for admin cilent management

--- resources/js/pages/admin/clients.tsx
+++ resources/js/pages/admin/clients.tsx
@@ -2,10 +2,11 @@ import AppLayout from "@/layouts/app-layout";
 import { clients } from "@/routes/admin";
 import { BreadcrumbItem } from "@/types";
 import { Client, columns } from "@/types/client";
-import { Button, Spinner, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, Tooltip, User } from "@heroui/react";
-import { Head } from "@inertiajs/react";
-import { Plus } from "lucide-react";
-import { useCallback, useEffect, useState } from "react";
+import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, Spinner, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, Tooltip, useDisclosure, User } from "@heroui/react";
+import { Head, useForm } from "@inertiajs/react";
+import { Eye, Plus, Trash2 } from "lucide-react";
+import { useCallback, useState } from "react";
+import { ClientForm } from "./createnew";
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -14,61 +15,88 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
-const rows = [
-    {
-        id: 1,
-        company_name: 'John Doe',
-        email: 'test@test.com',
-    },
-];
-
-export default function Clients() {
-    const [clients, setClients] = useState<Client[]>([]);
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        setIsLoading(true);
-        const fetchClients = async () => {
-            try {
-                const response = await fetch('/api/clients');
-                const data = await response.json();
-                setClients(data);
-            } catch (error) {
-                console.error('Error fetching clients:', error);
-            } finally {
-                setIsLoading(false);
-            }
-        };
-        fetchClients();
-    }, []);
+export default function Clients({ listClients } : { listClients: Client[] }) {
+    const [selectedClient, setSelectedClient] = useState<Client | undefined>();
+    const [isLoading, setIsLoading] = useState(false);
+    const {isOpen, onOpen, onOpenChange, onClose} = useDisclosure();
+    const confirmDeleteModal = useDisclosure();
+    const form = useForm<Client>({
+        company_name: '',
+        email: '',
+        address: '',
+        phone: '',
+        vat_number: '',
+    });
 
     const renderCell = useCallback((item: Client, columnKey: React.Key) => {
         const cellValue = item[columnKey as keyof Client];
 
         switch (columnKey) {
-        case "nome":
-            return (
-            <User description={item.email} name={cellValue}>
-                {item.email}
-            </User>
-            );
-        case "actions":
-            return (
-            <div className="relative flex items-center gap-2">
-                <Tooltip content="Dettagli">
-                <button onClick={() => console.log("Edit", item.id)}>👁️</button>
-                </Tooltip>
-                <Tooltip color="danger" content="Elimina">
-                <button onClick={() => console.log("Delete", item.id)}>🗑️</button>
-                </Tooltip>
-            </div>
-            );
-        default:
-            return cellValue;
+            case "actions":
+                return (
+                    <div className="relative flex items-center gap-2">
+                        <Tooltip content="Dettagli">
+                            <Button
+                                endContent={<Eye />}
+                                color="secondary"
+                                onPress={() => handleOpenModal(item)}
+                            />
+                        </Tooltip>
+                        <Tooltip color="danger" content="Elimina">
+                            <Button
+                                color="danger"
+                                endContent={<Trash2 />}
+                                onPress={() => {
+                                    setSelectedClient(item);
+                                    confirmDeleteModal.onOpen()
+                                }}
+                            />
+                        </Tooltip>
+                    </div>
+                );
+            default:
+                return cellValue;
         }
     }, []);
 
-    
+    const handleOpenModal = (client?: Client) => {
+        setSelectedClient(client);
+        onOpen();
+    };
+
+    const handleDelete = () => {
+        if (selectedClient) {
+            console.log("Deleting...");
+            form.delete(`/clients/${selectedClient.id}`, {
+                onSuccess: () => {
+                    console.log("Deleted");
+                    confirmDeleteModal.onClose();
+                }
+            });
+        }
+    }
+
+    const handleSave = () => {
+        if (selectedClient) {
+            console.log("Inizio l'update");
+            form.put(`/clients/${selectedClient.id}`, {
+                onSuccess: () => {
+                    console.log("Salvataggio terminato");
+                    onClose();
+                },
+                onError: (err) => {
+                    console.log("Sono andato in errore: ", err);
+                }
+            });
+        } else {
+            form.post('/clients', {
+                onSuccess: () => {
+                    onClose();
+                }
+            });
+        }
+    };
+
     return (
          <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Clients" />
@@ -78,6 +106,7 @@ export default function Clients() {
                         <Button
                             color="primary"
                             endContent={<Plus />}
+                            onPress={() => handleOpenModal()}
                         >
                             Add
                         </Button>
@@ -88,7 +117,7 @@ export default function Clients() {
                         {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
                     </TableHeader>
                     <TableBody
-                        items={clients}
+                        items={listClients}
                         emptyContent="Nessun cliente trovato"
                         loadingContent={<Spinner label="Loading..." />}
                         isLoading={isLoading}
@@ -101,6 +130,48 @@ export default function Clients() {
                     </TableBody>
                 </Table>
             </div>
+            <Modal
+                isOpen={isOpen}
+                onOpenChange={onOpenChange}
+            >
+                <ModalContent>
+                    {(onClose) => (
+                        <>
+                            <ModalHeader>{selectedClient ? "Modifica" : "Nuovo"} Cliente</ModalHeader>
+                            <ClientForm
+                                form={form}
+                                initialData={selectedClient}
+                                onSave={handleSave}
+                                onClose={onClose}
+                            />
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
+            <Modal
+                isOpen={confirmDeleteModal.isOpen}
+                onOpenChange={confirmDeleteModal.onOpenChange}
+            >
+                <ModalContent>
+                    <ModalHeader>
+                        Sei sicuro di voler cancellare il cliente?
+                    </ModalHeader>
+                    <ModalBody>
+
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button variant="flat" onPress={confirmDeleteModal.onClose}>
+                            Annulla
+                        </Button>
+                        <Button
+                            color="danger"
+                            onPress={ () => handleDelete() }
+                        >
+                            Conferma
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </AppLayout>
     );
 }
---
+++ resources/js/pages/admin/createnew.tsx
@@ -0,0 +1,102 @@
+import { Client } from "@/types/client";
+import { Button, Form, Input, ModalBody, ModalFooter } from "@heroui/react";
+import { useForm } from "@inertiajs/react";
+import { useEffect, useState } from "react";
+
+interface ClientFormProps {
+    form: ReturnType<typeof useForm<Client>>;
+    initialData: Client | undefined;
+    onSave: () => void;
+    onClose: () => void;
+}
+
+export const ClientForm = ({ form, initialData, onSave, onClose }: ClientFormProps) => {
+    const { data, setData, errors, processing } = form;
+
+    if (initialData) {
+        useEffect(() => {
+            setData({
+                'company_name': initialData.company_name,
+                'email': initialData.email,
+                'address': initialData.address,
+                'phone': initialData.phone,
+                'vat_number': initialData.vat_number,
+            });
+        }, [initialData]);
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(data);
+        onSave();
+    };
+
+    return (
+        <Form
+            onSubmit={handleSubmit}
+            className="w-full"
+        >
+            <ModalBody>
+                <div className="flex flex-col gap-4 w-full">
+                    <Input
+                        name="company_name"
+                        label="Ragione sociale"
+                        variant="bordered"
+                        isRequired
+                        defaultValue={initialData?.company_name}
+                        onValueChange={(v) => setData('company_name', v)}
+                        errorMessage="La Ragione Sociale è obbligatoria"
+                    />
+                    <Input
+                        name="email"
+                        label="Email"
+                        variant="bordered"
+                        type="email"
+                        isRequired
+                        defaultValue={initialData?.email}
+                        onValueChange={(v) => setData('email', v)}
+                        errorMessage="L'Email è obbligatoria"
+                    />
+                    <Input
+                        name="address"
+                        label="Indirizzo"
+                        variant="bordered"
+                        isRequired
+                        defaultValue={initialData?.address}
+                        onValueChange={(v) => setData('address', v)}
+                        errorMessage="L'Indirizzo è obbligatorio"
+                    />
+                    <Input
+                        name="phone"
+                        label="Telefono"
+                        variant="bordered"
+                        isRequired
+                        defaultValue={initialData?.phone}
+                        onValueChange={(v) => setData('phone', v)}
+                        errorMessage="Il numero di telefono è obbligatorio"
+                    />
+                    <Input
+                        name="vat_number"
+                        label="Partita IVA"
+                        variant="bordered"
+                        isRequired
+                        defaultValue={initialData?.vat_number}
+                        onValueChange={(v) => setData('vat_number', v)}
+                        errorMessage="La pratita IVA è obbligatoria"
+                    />
+                </div>
+            </ModalBody>
+            <ModalFooter>
+                <Button variant="flat" onPress={onClose}>
+                    Annulla
+                </Button>
+                <Button
+                    color="primary"
+                    type="submit"
+                >
+                    {initialData ? "Aggiorna" : "Salva"}
+                </Button>
+            </ModalFooter>
+        </Form>
+    );
+}
